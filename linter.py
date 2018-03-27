be9ba4f54a110507cac8923ea3837121e780db82
Koen Lageveen <koen.lageveen@me.com>
2018-03-27 08:12:11 +0200

    clean up, use selector

--- linter.py
+++ linter.py
@@ -1,24 +1,11 @@
-#
-# linter.py
-# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
-#
-# Written by Aparajita Fishman
-# Copyright (c) 2015-2016 The SublimeLinter Community
-# Copyright (c) 2014 Aparajita Fishman
-#
-# License: MIT
-#
-
-"""This module exports the Xmllint plugin class."""
-
 from SublimeLinter.lint import Linter, util
 
 
 class Xmllint(Linter):
-    """Provides an interface to xmllint."""
-
-    syntax = 'xml'
-    cmd = 'xmllint --noout * -'
+    cmd = 'xmllint --noout ${args} -'
     regex = r'^-:(?P<line>\d+):.+?: (?P<message>[^\r\n]+)(\r?\n[^\r\n]*\r?\n(?P<col>[^\^]*)\^)?'
     multiline = True
     error_stream = util.STREAM_STDERR
+    defaults = {
+        'selector': 'text.xml'
+    }
